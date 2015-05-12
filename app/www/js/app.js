5cc382670d8357275f57797fe7a8ab6a6fd68d8b
Justin Kazemi <justinkazemi@gmail.com>
2015-05-11 23:46:46 -0500

    please work for christs sake

--- app/www/js/app.js
+++ app/www/js/app.js
@@ -11,6 +11,7 @@ angular.module('app', [
 
 .run(function($ionicPlatform) {
     $ionicPlatform.ready(function() {
+      console.log('please dont mind this console');
       // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
       // for form inputs)
       if (window.cordova && window.cordova.plugins.Keyboard) {
